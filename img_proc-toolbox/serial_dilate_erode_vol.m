e188948dbad00cf59702eb84c1e19e30b0b90489
Jian-Qing Zheng <jianqingzheng@users.noreply.github.com>
2023-04-04 22:16:00 +0100

    1

--- img_proc-toolbox/serial_dilate_erode_vol.m
+++ img_proc-toolbox/serial_dilate_erode_vol.m
@@ -1,9 +1,15 @@
-function out_vol=serial_dilate_erode_vol(vol,dilate_erode_rates,growing_msk)
-if nargin<3
-    growing_msk=1;
+function out_vol=serial_dilate_erode_vol(vol,dilate_erode_rates,growing_msk,min_area,se_type)
+if nargin<5
+    se_type='sphere';
+    if nargin<4
+        min_area=0;
+        if nargin<3
+            growing_msk=1;
+        end
+    end
 end
 for r= dilate_erode_rates
-    se=strel('sphere',abs(r));
+    se=strel(se_type,abs(r));
 %     se=strel('cube',abs(r));
     if r>0
         vol=imdilate(vol,se);
@@ -11,6 +17,9 @@ for r= dilate_erode_rates
         vol=imerode(vol,se);
     end
     vol=vol.*growing_msk;
+    if min_area>0
+        vol=bwareaopen(vol,min_area);
+    end
 end
 out_vol=vol;
 end
